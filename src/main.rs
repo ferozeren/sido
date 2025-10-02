5cbb40ee08fca59dd6439ecd72a637ee41a78be7
ferozeren <ferozeren@proton.me>
2025-10-02 23:57:39 +0530

    initial slint-ui setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui: AppWindow = AppWindow::new()?;
+
+    ui.run()?;
+    Ok(())
 }
